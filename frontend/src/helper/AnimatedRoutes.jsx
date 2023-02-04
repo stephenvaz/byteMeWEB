9d61248c50d674612b1f0710de6f758ebab27cb5
Stephen Vaz <54773712+stephenvaz@users.noreply.github.com>
2023-02-04 11:25:16 +0530

    frontend init commit

---
+++ frontend/src/helper/AnimatedRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion';
+import Login from '../components/Login'
+import EventAdd from '../components/EventAdd'
+import InitialTransition from '../components/Loading';
+
+function AnimatedRoutes() {
+    const location = useLocation()
+    return (
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<InitialTransition/>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/eventadd" element={<EventAdd />} />
+            </Routes>
+        </AnimatePresence>
+    )
+}
+
+export default AnimatedRoutes
