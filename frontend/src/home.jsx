1a560cf6858f19e0212039785f1f081af7868dc2
Vi-shub <Smsharma3121@gmail.com>
2023-02-05 05:20:53 +0530

    navbar link active

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -9,12 +9,28 @@ import ImageTwo from './parallax/imgTwo';
 
 function Home() {
   return (
+<<<<<<< HEAD
     <div className='back'>
         <NavB />
       <div className='bigbox'>
         <BootstrapCarousel />
-        <Tile />
+        <div id='section2'>
+          <Tile />
+        </div>
+        
       </div>
+=======
+    // <div className='back'>
+    //     <NavB />
+    //   <div className='bigbox'>
+    //     <BootstrapCarousel />
+    //     <Tile />
+    //   </div>
+    // </div>
+    <div>
+    <ImageOne />
+    <ImageTwo />
+>>>>>>> e81b79012a1aff70bc258bd3a91d7c7cf4146a58
     </div>
   )
 }
